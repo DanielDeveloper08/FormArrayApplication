48b12fa50adf4e89d7ac4e7aff0b0d14107d719b
Daniel Asanza Erazo <danielprogramando2000@gmail.com>
2023-03-28 00:40:10 -0500

    FormArray funcionando con datos inicializados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FormArrayPractice';
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  form!: FormGroup;
+
+  arrayData:any = [
+    {title: 'Angular', level: 'beginner'},
+    {title: 'React', level: 'intermediate'},
+    {title: 'Vue', level: 'advanced'}
+  ];
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      lessons: this.formBuilder.array([])
+    });
+
+    this.arrayData.forEach((data: { title: any; level: any; }) => {
+      const lessonForm = this.formBuilder.group({
+        title: [data.title, Validators.required],
+        level: [data.level, Validators.required]
+      });
+      this.lessons.push(lessonForm);
+    });
+  }
+  
+  get lessons() {
+    return this.form.controls['lessons'] as FormArray;
+  }
+  
+  addLesson() {
+    const lessonForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      level: ['', Validators.required]
+    });
+  
+    this.lessons.push(lessonForm);
+  }
+  
+  deleteLesson(index: number) {
+    this.lessons.removeAt(index);
+  }
+
+  saveLesson() {
+    console.log(this.form.value);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
